38da20c600d7aa142fc76b5d98ffc539a8033a35
Ayla-Solo <o.soloveva2@g.nsu.ru>
2026-04-18 00:10:36 +0700

    ex_13.py

---
+++ ex_13.py
@@ -0,0 +1,125 @@
+import turtle as t
+
+screen = t.Screen()
+screen.setup(width = 800, height = 800)
+t.speed(0)
+t.hideturtle()
+
+C2 = "#E0F7FA"  # Светло-голубой
+C3 = "#4FC3F7"  # Средний голубой
+C4 = "#0288D1"  # Темно-синий
+
+def draw_triangle(p1, p2, p3, color):
+    t.penup()
+    t.goto(p1)
+    t.pendown()
+    t.fillcolor(color)
+    t.pencolor("white")
+    t.pensize(2)
+    t.begin_fill()
+    t.goto(p2)
+    t.goto(p3)
+    t.goto(p1)
+    t.end_fill()
+
+def draw_tile(x, y, size, color1, color2, diag_type):
+    tl = (x, y)
+    tr = (x + size, y)
+    bl = (x, y - size)
+    br = (x + size, y - size)
+
+    if diag_type == 0:  #(\)
+        draw_triangle(tl, tr, br, color1)
+        draw_triangle(tl, bl, br, color2)
+    else:  #(/)
+        draw_triangle(tl, tr, bl, color2)
+        draw_triangle(tr, br, bl, color1)
+
+def draw_pattern():
+    size = 70
+    rows = 6
+    cols = 6
+    start_x = - (cols * size) / 2
+    start_y = (rows * size) / 2
+
+    for row in range(rows):
+        for col in range(cols):
+            x = start_x + col * size
+            y = start_y - row * size
+
+            if row == 2 and col == 2:
+                draw_tile(x, y, size, C4, C3, 0)
+            elif row == 2 and col == 3:
+                draw_tile(x, y, size, C3, C4, 1)
+            elif row == 3 and col == 2:
+                 draw_tile(x, y, size, C4, C3, 1)
+            elif row == 3 and col == 3:
+                draw_tile(x, y, size, C3, C4, 0)
+            else:
+                if row == 0 and col == 0:
+                    draw_tile(x, y, size, C4, C3, 0)
+                elif row == 0 and col == 1:
+                    draw_tile(x, y, size, C3, C4, 0)
+                elif row == 0 and col == 2:
+                    draw_tile(x, y, size, C2, C3, 0)
+                elif row == 0 and col == 3:
+                    draw_tile(x, y, size, C3, C2, 1)
+                elif row == 0 and col == 4:
+                    draw_tile(x, y, size, C4, C3, 1)
+                elif row == 0 and col == 5:
+                    draw_tile(x, y, size, C3, C4, 1)
+                elif row == 1 and col == 0:
+                    draw_tile(x, y, size, C3, C2, 0)
+                elif row == 1 and col == 1:
+                    draw_tile(x, y, size, C4, C3, 0)
+                elif row == 1 and col == 2:
+                    draw_tile(x, y, size, C3, C4, 0)
+                elif row == 1 and col == 3:
+                    draw_tile(x, y, size, C4, C3, 1)
+                elif row == 1 and col == 4:
+                    draw_tile(x, y, size, C3, C4, 1)
+                elif row == 1 and col == 5:
+                    draw_tile(x, y, size, C2, C3, 1)
+                elif row == 2 and col == 0:
+                    draw_tile(x, y, size, C2, C3, 0)
+                elif row == 2 and col == 1:
+                    draw_tile(x, y, size, C3, C2, 0)
+                elif row == 2 and col == 4:
+                    draw_tile(x, y, size, C2, C3, 1)
+                elif row == 2 and col == 5:
+                    draw_tile(x, y, size, C3, C2, 1)
+                elif row == 3 and col == 0:
+                    draw_tile(x, y, size, C2, C3, 1)
+                elif row == 3 and col == 1:
+                    draw_tile(x, y, size, C3, C2, 1)
+                elif row == 3 and col == 4:
+                    draw_tile(x, y, size, C2, C3, 0)
+                elif row == 3 and col == 5:
+                    draw_tile(x, y, size, C3, C2, 0)
+                elif row == 4 and col == 0:
+                    draw_tile(x, y, size, C3, C2, 1)
+                elif row == 4 and col == 1:
+                    draw_tile(x, y, size, C4, C3, 1)
+                elif row == 4 and col == 2:
+                    draw_tile(x, y, size, C3, C4, 1)
+                elif row == 4 and col == 3:
+                    draw_tile(x, y, size, C4, C3, 0)
+                elif row == 4 and col == 4:
+                    draw_tile(x, y, size, C3, C4, 0)
+                elif row == 4 and col == 5:
+                    draw_tile(x, y, size, C2, C3, 0)
+                elif row == 5 and col == 0:
+                    draw_tile(x, y, size, C4, C3, 1)
+                elif row == 5 and col == 1:
+                    draw_tile(x, y, size, C3, C4, 1)
+                elif row == 5 and col == 2:
+                    draw_tile(x, y, size, C2, C3, 1)
+                elif row == 5 and col == 3:
+                    draw_tile(x, y, size, C3, C2, 0)
+                elif row == 5 and col == 4:
+                    draw_tile(x, y, size, C4, C3, 0)
+                elif row == 5 and col == 5:
+                    draw_tile(x, y, size, C3, C4, 0)
+
+draw_pattern()
+t.done()
